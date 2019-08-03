824a7d0c5e14c8df3beb0818b315f1e61098b68b
Glenn Murray <evocativeu@icloud.com>
2019-08-03 13:46:41 -0700

    First commit

---
+++ Assets/ArtworkController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ArtworkController : MonoBehaviour
+{
+    public GameObject image;
+
+    public void SetArtInfo(ArtInfo info)
+    {
+        Renderer rend = image.GetComponent<Renderer>();
+        Material material = rend.material;
+        material.mainTexture = info.image;
+    }
+}
---
+++ Assets/ScriptableObjects/ArtInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "My Objects/Art Info")]
+public class ArtInfo : ScriptableObject
+{
+    public Texture image;
+    public string title;
+    public string artist;
+    [Multiline]
+    public string description;
+}
