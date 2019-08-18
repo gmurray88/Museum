095621af239b883d579f3038b4e221bc723f56f2
Glenn Murray <evocativeu@icloud.com>
2019-08-17 23:19:54 -0700

    add dynamic pic sizing - stage 1

--- Assets/ArtworkController.cs
+++ Assets/ArtworkController.cs
@@ -3,11 +3,29 @@
 public class ArtworkController : MonoBehaviour
 {
     public GameObject image;
+    public Transform frame;
 
     public void SetArtInfo(ArtInfo info)
     {
         Renderer rend = image.GetComponent<Renderer>();
         Material material = rend.material;
         material.mainTexture = info.image;
+        Debug.Log("Pre method Image Height " + info.orig_height);
+        Debug.Log("Pre method Image Width " + info.orig_width);
+        frame.localScale = TextureToScale(info.orig_height, info.orig_width, frame.localScale.z);
+
+    }
+
+    private Vector3 TextureToScale(float height, float width, float depth)
+    {
+        Vector3 scale = Vector3.one;
+        scale.z = depth;
+        Debug.Log("Orig Image Height " + height);
+        Debug.Log("Orig Image Width " + width);
+
+        scale.y = (float)((height * 1.05) / 36);
+        scale.x = (float)((width * 1.05) / 36);
+
+        return scale;
     }
 }
--- Assets/ScriptableObjects/ArtInfo.cs
+++ Assets/ScriptableObjects/ArtInfo.cs
@@ -6,6 +6,8 @@ public class ArtInfo : ScriptableObject
     public Texture image;
     public string title;
     public string artist;
+    public float orig_height;
+    public float orig_width;
     [Multiline]
     public string description;
 }
